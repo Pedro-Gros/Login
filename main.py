a5dcd57f78220b17e047b0f319baa7d988354ef3
Pedro Gros <pedro_gros1998@hotmail.com>
2022-02-02 15:50:06 -0300

    iniciando tela logado

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from telnetlib import LOGOUT
 from PyQt6 import uic,QtWidgets
 from CrudLogin import Login
 
@@ -8,6 +9,8 @@ def login():
 
     if Login().logar(usuario, senha) == 0:
         tela_login.close()
+        tela_main.show()
+        
     elif Login().logar(usuario, senha) == 1:
         tela_login.frame_erro.show()
         tela_login.label_erro.setText("Usuario Incorreto")
@@ -15,11 +18,18 @@ def login():
         tela_login.frame_erro.show()
         tela_login.label_erro.setText("Senha Incorreta")
 
+def logout():
+    tela_login.lineedit_user.clear()
+    tela_login.lineedit_senha.clear()
+    tela_main.close()
+    tela_login.show()
 
 
 app = QtWidgets.QApplication([])
 tela_login = uic.loadUi("Login.ui")
+tela_main = uic.loadUi("Main.ui")
 tela_login.pushButton_entrar.clicked.connect(login)
+tela_main.bt_sair.clicked.connect(logout)
 tela_login.frame_erro.hide()
 
 tela_login.show()
